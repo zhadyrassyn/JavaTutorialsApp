9cc1558014c4fb37ddd03a3014448fb2edbb9911
Daniyar Zhadyrassyn <zhadyrassyn.daniyar@is.sdu.edu.kz>
2018-01-07 18:02:18 +0600

    Initial commit

---
+++ app/src/main/java/java/tutorials/zhadyrassyn/com/javatutorials/MainActivity.java
@@ -0,0 +1,13 @@
+package java.tutorials.zhadyrassyn.com.javatutorials;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
